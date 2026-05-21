baac4e9b4e08a7635432b9161653ef253882d608
Irvin Pineda <00024420@uca.edu.sv>
2026-05-20 21:09:57 -0600

    Fix bug

--- src/main/java/org/example/labo03/controllers/SpecimenController.java
+++ src/main/java/org/example/labo03/controllers/SpecimenController.java
@@ -14,7 +14,7 @@ import java.time.LocalDateTime;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/api/v1/specimens")
+@RequestMapping("/api/specimens")
 @RequiredArgsConstructor
 public class SpecimenController {
 
